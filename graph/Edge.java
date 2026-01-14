db85bc7eadf869f1c027ad3c1017dca346f04e62
avi1967 <184269098+avi1967@users.noreply.github.com>
2026-01-14 19:03:40 +0530

    Add Edge class for undirected weighted graph

---
+++ graph/Edge.java
@@ -0,0 +1,13 @@
+// Data Structure: Edge (Undirected Weighted Graph)
+
+package graphs;
+
+public class Edge {
+    public int u, v, weight;
+
+    public Edge(int u, int v, int weight) {
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+}
